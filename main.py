1ebe44bb2763edac13e9351fea86458e6dd7fd4d
sdhamodaran <sdhamodaran@gmail.com>
2013-06-16 12:03:45 -0700

    Basic functionality

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python
-
+import time
 import Image
 import sys
-filename = "IMG_9435"
-image = Image.open(filename+".JPG")
-tile_width = 100
-tile_height = 80
+filename = "IMG_89"
+image = Image.open(filename+".jpg")
+tile_width = 180
+tile_height = 120
 zoom_level = 0
 count = 1
 currentx = 0
 currenty = 0
 while currenty < image.size[1]:
 	while currentx < image.size[0]:
-		print currentx,",",currenty
-		tile = image.crop((currentx,currenty,currentx + tile_width,currenty + tile_height))
+		print currentx,",",currenty, ",", currentx + tile_width, "," , currenty + tile_height
+        	tile = image.crop((currentx,currenty,currentx + tile_width,currenty + tile_height))
 		tile.save(filename+ "_"+str(count) + ".png","PNG")
 		currentx += tile_width
+                count += 1
 	currenty += tile_height
 	currentx = 0
 	count += 1
